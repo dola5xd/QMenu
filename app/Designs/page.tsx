9c99b12e994be7a7148f648da4e419e17fcbc945
dola5xd <dolay6253@gmail.com>
2025-08-14 15:50:23 +0300

    fix DesignSelection

--- app/Designs/page.tsx
+++ app/Designs/page.tsx
@@ -2,8 +2,8 @@ import { getServerSession } from "next-auth";
 import { getMenusByUser } from "@/_actions/getMenus";
 import { MenuData } from "@/_actions/createMenu";
 import FiltersBar from "@/_components/ui/Filters";
-import CreateMenuDialog from "@/_components/designs/DesignSelection";
 import { authOptions } from "@/_lib/authOptions";
+import CreateMenuDialog from "@/_components/designs/DesignSelection";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
@@ -22,7 +22,7 @@ export default async function Page() {
   }
 
   return (
-    <section className="px-4 md:px-5 lg:px-10 py-2">
+    <section className="px-4 py-2 md:px-5 lg:px-10">
       <header className="flex items-center justify-between md:pr-10">
         <h2 className="text-xl font-semibold">Your Menus</h2>
         <CreateMenuDialog />
